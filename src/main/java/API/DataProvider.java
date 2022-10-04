7ee83d669e38929882ac671f8a33a3d956dffba6
Luca Coduri <coduri.vitor@gmail.com>
2022-10-04 17:52:30 +0200

    Fetching data on "get" methods in Dataprovider

--- src/main/java/API/DataProvider.java
+++ src/main/java/API/DataProvider.java
@@ -7,9 +7,13 @@ import java.util.List;
 
 public abstract class DataProvider {
     List<Driver> getDrivers(){
+        String json = fetchDrivers();
+        // TODO convert json to object
         return null;
     }
     List<Car> getCars(){
+        String json = fetchCars();
+        // TODO convert json to object
         return null;
     }
 
