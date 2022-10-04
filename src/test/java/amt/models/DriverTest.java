19035e3a166d0da66c4311f70602d05342c800a4
Chloé <cfontaine@citycable.ch>
2022-10-04 17:52:32 +0200

    Add driver constructor + getters + setters tests

--- src/test/java/amt/models/DriverTest.java
+++ src/test/java/amt/models/DriverTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,11 +21,75 @@ class DriverTest {
     }
 
     @Test
-    void createDriver(){
+    void driverCreation_WithoutAttributes_Success() {
         final Driver driver = new Driver("id", "name");
+
 
         assertEquals("id", driver.getId());
         assertEquals("name", driver.getName());
-        assertEquals( 0, driver.getAttributes().size());
+        assertEquals(0, driver.getAttributes().size());
+    }
+
+    @Test
+    void driverCreation_WithAttributes_Success() {
+        final ArrayList<Attribute> attributes = new ArrayList<>();
+        attributes.add(new Attribute("attr1", "val1"));
+        final Driver driver = new Driver("id", "name", attributes);
+
+        assertEquals("id", driver.getId());
+        assertEquals("name", driver.getName());
+        assertEquals(1, driver.getAttributes().size());
+    }
+
+    @Test
+    void attributesLocalModification_DoesntAffectDriverAttributes_Success() {
+        final ArrayList<Attribute> attributes = new ArrayList<>();
+        attributes.add(new Attribute("attr1", "val1"));
+        final Driver driver = new Driver("id", "name", attributes);
+        attributes.remove(0);
+
+        assertEquals(1, driver.getAttributes().size());
+    }
+
+    @Test
+    void driverAttributes_GetAttributes_Success() {
+        final ArrayList<Attribute> attributes = new ArrayList<>();
+        attributes.add(new Attribute("attr1", "val1"));
+        final Driver driver = new Driver("id", "name", attributes);
+
+        assertEquals(attributes, driver.getAttributes());
+    }
+
+    @Test
+    void driverAttributes_GetAttributesModification_DoesntAffectDriverAttributes_Success() {
+        final ArrayList<Attribute> attributes = new ArrayList<>();
+        Attribute attr1 = new Attribute("attr1", "val1");
+        attributes.add(attr1);
+        final Driver driver = new Driver("id", "name", attributes);
+        driver.getAttributes().remove(attr1);
+
+        assertEquals(1, driver.getAttributes().size());
+    }
+
+    @Test
+    void driverAttributes_GetEmptyAttributes_ReturnsEmptyList_Success() {
+        final Driver driver = new Driver("id", "name");
+
+        assertEquals(new ArrayList<Attribute>(), driver.getAttributes());
+        assertEquals(0, driver.getAttributes().size());
+    }
+
+    @Test
+    void driverId_GetId_Success() {
+        final Driver driver = new Driver("id", "name");
+
+        assertEquals("id", driver.getId());
+    }
+
+    @Test
+    void driverName_GetName_Success() {
+        final Driver driver = new Driver("id", "name");
+
+        assertEquals("name", driver.getName());
     }
 }
